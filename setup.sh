0d6d4114ac7546f259bf79e9e32793ef89489be8
neerajnair19 <37244983+neerajnair19@users.noreply.github.com>
2021-05-21 13:21:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"blackberry19111999.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
